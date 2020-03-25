563fd7887f310fcfa1b01c2e548c96a964c40a71
Torsten Moss <tbm94@live.no>
2020-03-25 16:09:04 +0100

    Add favorite functionality and make responsive

--- src/App/AppProvider.js
+++ src/App/AppProvider.js
@@ -1,39 +1,68 @@
 import React from 'react';
+import _ from 'lodash';
 
 const cc = require('cryptocompare');
 
 export const AppContext = React.createContext();
 
+const MAX_FAVORITES = 10;
+
 export class AppProvider extends React.Component {
     constructor(props){
         super(props);
         this.state  = {
+            page: 'dashboard',
+            favorites: ['BTC', 'ETH', 'XMR', 'DOGE'],
             ...this.getSavedSettings(),
             setPage: this.setPage,
-            confirmFavorites: this.confirmFavorites
+            addCoin: this.addCoin,
+            removeCoin: this.removeCoin,
+            confirmFavorites: this.confirmFavorites,
+            isInFavorites: this.isInFavorites
+        }
+    } 
+
+    isInFavorites = key => _.includes(this.state.favorites, key);
+    
+    addCoin = key => {
+        let favorites = [...this.state.favorites];
+        if (favorites.length < MAX_FAVORITES){
+            favorites.push(key);
+            this.setState({favorites});
         }
     }
+
+    removeCoin = key => {
+        let favorites = [...this.state.favorites];
+        this.setState({favorites: _.pull(favorites, key)});
+    }
+
     componentDidMount(){
         this.fetchCoins();
     }
+
     fetchCoins = async () => {
         let coinList = (await cc.coinList()).Data;
         this.setState({coinList});
     }
+
     confirmFavorites = () => {
         this.setState({
             firstVisit: false,
+            page: 'dashboard'
         });
         localStorage.setItem('data', JSON.stringify({
-            test: 'hello'
+            favorites: this.state.favorites
         }))
     }
+
     getSavedSettings(){
         let data = JSON.parse(localStorage.getItem('data'));
         if (!data){
             return { page: "settings", firstVisit: true };
         } 
-        return {};
+        let favorites = data.favorites;
+        return {favorites};
     }
     
     setPage = page => this.setState({page});
